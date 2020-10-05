65fdb8d0a09808f7f4ee64957929096f4d20c1ea
DeathLess9 <devin0579@gmail.com>
2020-10-05 15:34:45 +1000

    merged changes of views.py and models.py

--- auction/models.py
+++ auction/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from datetime import datetime
 from . import db
 from flask_login import UserMixin
@@ -31,24 +30,3 @@ class Login:
     def __init__(self, email, pwd):
         self.email = email
         self.pwd = pwd
-=======
-from . import db
-
-class Item:
-    def __init__(self, name, description, image, currency):
-        self.name = name
-        self.description = description
-        self.image = image
-        self.currency = currency
-        self.comments = list()
-    
-    def set_comments(self, comment):
-        self.comments.append(comment)
-
-
-class Comment:
-    def __init__(self, user, text, created_at):
-        self.user = user
-        self.text = text
-        self.created_at = created_at
->>>>>>> c5cf62923cff0fcdc8c65f4d877580b4c1e08724
--- auction/views.py
+++ auction/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Blueprint, render_template, request, session, redirect, url_for
 from .models import Item, Login
 
@@ -23,17 +22,6 @@ def search():
         return render_template('index.html', items=items)
 
     return redirect(url_for('main.index'))
-=======
-from flask import Blueprint, render_template, request, session
-
-mainbp = Blueprint('main', __name__)
-
-@mainbp.route('/')
-def index():
-    print(request.values.get('email'))
-    print(request.values.get('pwd'))
-    return render_template('index.html')
->>>>>>> c5cf62923cff0fcdc8c65f4d877580b4c1e08724
 
 
 @mainbp.route('/watchlist')
