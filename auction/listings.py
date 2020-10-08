872d8114fa6906de2dff929e2f4a50ad678464f7
adaeo <you@example.com>
2020-10-08 17:15:25 +1000

    BugFixes

--- auction/listings.py
+++ auction/listings.py
@@ -57,14 +57,13 @@ def listings():
 def search():
     category_form = CategoryForm()
     if category_form.validate_on_submit():
-        if category_form.category.data != 'None':
-            cat = category_form.category.data
-            items = Item.query.filter(Item.category == cat).all()
-            return render_template('listing/listings.html', form = category_form, items = items)
-        else:
+        cat = category_form.category.data
+        if cat == 'None':
             items = Item.query.all()
             return render_template('listing/listings.html', form = category_form, items = items)
-
+        else:
+            items = Item.query.filter(Item.category == cat).all()
+            return render_template('listing/listings.html', form = category_form, items = items)
     else:
         items = Item.query.all()
         return render_template('listing/listings.html', form = category_form, items = items)
