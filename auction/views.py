b090e619e57e6be5e46cdbeabb1646c7dff9b398
DeathLess9 <devin0579@gmail.com>
2020-10-08 15:14:18 +1000

    Created search route in views.py

--- auction/views.py
+++ auction/views.py
@@ -4,12 +4,25 @@ from .models import Item
 
 mainbp = Blueprint('main', __name__)
 
+
 @mainbp.route('/')
 def index():
     print(request.values.get('email'))
     print(request.values.get('pwd'))
     items = Item.query.all()
     return render_template('index.html', items=items)
+
+
+@mainbp.route('/search')
+def search():
+    if request.args['search']:
+        item = "%" + request.args['search'] + '%'
+        items = Item.query.filter(
+            Item.name.like(item)).all()
+        return render_template('index.html', items=items)
+    else:
+        return redirect(url_for('main.index'))
+
 
 @mainbp.route('/watchlist')
 def watchlist():
