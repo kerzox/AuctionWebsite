d597f193d626e9a8aae08029b28bb3ca19ddce48
DeathLess9 <devin0579@gmail.com>
2020-10-12 16:10:26 +1000

    Attempted to incorporate mylisting feature

--- auction/listings.py
+++ auction/listings.py
@@ -2,33 +2,33 @@ from flask import Blueprint, render_template, request, session, url_for, redirec
 from .forms import CreateListingForm, CategoryForm
 from .models import Item
 from flask_login import login_required, current_user
-from werkzeug.utils import secure_filename 
+from werkzeug.utils import secure_filename
 from . import db
 import os
+import sqlite3
 
 listingbp = Blueprint('listing', __name__, url_prefix='/listings')
+
 
 @listingbp.route('/item/<id>')
 def item(id):
     item = Item.query.filter_by(id=id).first()
     return render_template('listing/item.html', item=item)
 
-@listingbp.route('/mylistings')
-@login_required
-def mylistings():
-    return render_template('listing/mylistings.html')
 
 def check_upload_file(form):
     fp = form.image.data
     filename = fp.filename
     BASE_PATH = os.path.dirname(__file__)
 
-    upload_path = os.path.join(BASE_PATH, 'static/image', secure_filename(filename))
-    db_upload_path='/static/image/' + secure_filename(filename)
+    upload_path = os.path.join(
+        BASE_PATH, 'static/image', secure_filename(filename))
+    db_upload_path = '/static/image/' + secure_filename(filename)
     fp.save(upload_path)
     return db_upload_path
 
-@listingbp.route('/create', methods = ['GET', 'POST'])
+
+@listingbp.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
     create_form = CreateListingForm()
@@ -46,24 +46,42 @@ def create():
         return redirect(url_for('listing.mylistings'))
     return render_template('listing/create.html', form=create_form)
 
+
 @listingbp.route('/listings')
 def listings():
 
     items = Item.query.all()
     category_form = CategoryForm()
-    return render_template('listing/listings.html', form = category_form, items = items)
+    return render_template('listing/listings.html', form=category_form, items=items)
 
-@listingbp.route('/search', methods = ['GET', 'POST'])
+
+def get_my_list(user_id):
+    conn = sqlite3.connect('SQLALCHEMY_DATABASE_URI')
+    get = conn.execute(
+        'SELECT * FROM items WHERE user_id = ?', (user_id,)).fetchall()
+    conn.close()
+    return get
+
+
+@listingbp.route('/mylistings')
+@login_required
+def mylistings(user_id):
+    mylist = get_my_list(user_id)
+    category_form = CategoryForm()
+    return render_template('listing/mylistings.html', form=category_form, mylist=mylist)
+
+
+@listingbp.route('/search', methods=['GET', 'POST'])
 def search():
     category_form = CategoryForm()
     if category_form.validate_on_submit():
         cat = category_form.category.data
         if cat == 'None':
             items = Item.query.all()
-            return render_template('listing/listings.html', form = category_form, items = items)
+            return render_template('listing/listings.html', form=category_form, items=items)
         else:
             items = Item.query.filter(Item.category == cat).all()
-            return render_template('listing/listings.html', form = category_form, items = items)
+            return render_template('listing/listings.html', form=category_form, items=items)
     else:
         items = Item.query.all()
-        return render_template('listing/listings.html', form = category_form, items = items)
+        return render_template('listing/listings.html', form=category_form, items=items)
