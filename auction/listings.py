a4bb9f77a0037f842cb7be56523fc73a91fbd53d
William <56326493+kerzox@users.noreply.github.com>
2020-10-19 10:19:27 +1000

    Create route now actually adds user_id foreign key to the database

--- auction/listings.py
+++ auction/listings.py
@@ -38,7 +38,8 @@ def create():
                        category=create_form.category.data,
                        description=create_form.description.data,
                        image=db_file_path,
-                       start_currency=create_form.start_bid.data)
+                       start_currency=create_form.start_bid.data,
+                       users=current_user)
 
         print('Listing has been created', 'success')
         db.session.add(listing)
