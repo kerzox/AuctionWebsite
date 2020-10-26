8f208e7851c54a59110d2741baf0e5cd2ff4bd48
adaeo <you@example.com>
2020-10-26 14:43:12 +1000

    Changes to index listings func

--- auction/views.py
+++ auction/views.py
@@ -11,7 +11,7 @@ mainbp = Blueprint('main', __name__)
 def index():
     print(request.values.get('email'))
     print(request.values.get('pwd'))
-    items = Item.query.limit(6).order_by(Item.id.desc()).all()
+    items = Item.query.order_by(Item.id.desc()).limit(6).all()
     return render_template('index.html', items=items)
 
 
