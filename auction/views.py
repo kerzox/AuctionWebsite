b3f939d1f51441afa81573603acef19b6d357a26
adaeo <you@example.com>
2020-10-26 15:48:50 +1000

    Testing categories

--- auction/views.py
+++ auction/views.py
@@ -12,20 +12,8 @@ def index():
     print(request.values.get('email'))
     print(request.values.get('pwd'))
     items = Item.query.order_by(Item.id.desc()).limit(6).all()
-    bid = Bids.query.order_by(Bids.user_id.desc()).limit(6).all()
 
     return render_template('index.html', items=items, bid=bid)
-
-
-@mainbp.route('/search')
-def search():
-    if request.args['search']:
-        item = "%" + request.args['search'] + '%'
-        items = Item.query.filter(
-            Item.name.like(item)).all()
-        return render_template('index.html', items=items)
-    else:
-        return redirect(url_for('main.index'))
 
 
 @mainbp.route('/watchlist')
