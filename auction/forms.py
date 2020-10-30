1fbfeddd15af58d61e64b1ac0f88d6f069d0cba0
DeathLess9 <devin0579@gmail.com>
2020-10-30 13:53:48 +1000

    Removed changes as loginrequired already in place

--- auction/forms.py
+++ auction/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField, DecimalField, SelectField
 from wtforms.validators import InputRequired, Length, EqualTo, Email, NumberRange, Regexp
 from flask_wtf.file import FileRequired, FileField, FileAllowed, DataRequired
-from flask_login import login_required
 from . import db
 
 ALLOWED_FILE = {'png', 'jpg', 'JPG', 'PNG'}  
@@ -25,7 +24,6 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Register")
 
 class AddBidForm(FlaskForm):
-    decorators = [login_required]
     bid_amount = DecimalField('Bid amount', validators=[InputRequired('Bid is required'), NumberRange(min=1, max=99999999, message='Value must bebetween $1 and $99999999')])
     submit = SubmitField("Add")
 
