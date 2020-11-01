58f41b6eb9306da9c66f50f3054f251601153fec
kerzox <williampappin@gmail.com>
2020-11-01 12:32:33 +1000

    Added 500 and revised 404 error handling

--- auction/__init__.py
+++ auction/__init__.py
@@ -3,26 +3,33 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-from werkzeug.exceptions import HTTPException
+from werkzeug.exceptions import HTTPException, InternalServerError, NotFound
 
 db=SQLAlchemy()
 
-# inbuilt function which takes error as parameter
-def not_found(e):
-# defining function
-    return render_template("authentication/404.html")
 
 def create_app():
     print(__name__)
     app = Flask(__name__)
-    app.register_error_handler(404, not_found)
     app.secret_key = 'Secret'
+
+    @app.errorhandler(InternalServerError)
+    def internal_server_error(e):
+        # note that we set the 500 status explicitly
+        return render_template('authentication/500.html')
+
+    @app.errorhandler(NotFound)
+    def not_found(e):
+        # defining function
+        return render_template("authentication/404.html")
+    
+
     
     # Heroku postgresql
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
     # local DB
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travel.sqlite'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travel.sqlite'
 
     #initialize postgres
     db.init_app(app)
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from auction import create_app
 
 if __name__ == '__main__':
     napp = create_app()
-    napp.run(debug = True)
+    napp.run(debug = False)
 
